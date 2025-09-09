131d11fd1a263d00ece89bd5da9a5c55bfcc6dc3
Manelisi1972 <ST10447923@rcconnect.edu.za>
2025-09-09 17:22:27 +0000

    Added two new separate controllers which are Claims Controller and Admin Controller, in HomeControllers.cs                             Created a new Folder "claims" under Views folder.  Created a new Razor View "Create.cshtml" for submitting claims

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,6 +25,36 @@ namespace ContractMonthlyClaimSystems.Controllers
         }
     }
 
+    public class ClaimsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // List of claims
+        }
+
+        public IActionResult Create()
+        {
+            return View(); // Submit new claim
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(); // Claim details + status
+        }
+
+        public IActionResult Upload()
+        {
+            return View(); // Upload documents
+        }
+    }
+
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // Coordinator/Manager dashboard
+        }
+    }
 
 
 }
